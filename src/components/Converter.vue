9089a9731e85e1906c18f841ce4f9f2f985bae9b
Zubs <zubairidrisaweda@gmail.com>
2021-03-10 23:21:50 +0100

    Done, but a few validations left

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -48,24 +48,83 @@
 
     methods: {
       convert () {
+
+        // Remove previous result
+        this.result = null;
         
         // Validate inputs
         if (this.$refs.form.validate()) {
-          
-          // Convert Decimal
-          if (this.baseFrom == this.bases[2]) {
 
-            // To Binary
-            if (this.baseTo == this.bases[0]) {
-              this.result = this.decimalToBinary(this.input);
-            }
+          // Lemme try this
+          switch (this.baseFrom) {
+            case this.bases[0]:
+              switch (this.baseTo) {
+                case this.bases[0]:
+                  this.result = this.input;
+                  break;
+                case this.bases[1]:
+                  this.result = parseInt(this.input, 2).toString(8);
+                  break;
+                case this.bases[2]:
+                  this.result = parseInt(this.input, 2).toString(10);
+                  break;
+                case this.bases[3]:
+                  this.result = parseInt(this.input, 2).toString(16);
+                  break;
+              }
+              break;
+            case this.bases[1]:
+              switch (this.baseTo) {
+                case this.bases[0]:
+                  this.result = parseInt(this.input, 8).toString(2);
+                  break;
+                case this.bases[1]:
+                  this.result = this.input;
+                  break;
+                case this.bases[2]:
+                  this.result = parseInt(this.input, 8).toString(10);
+                  break;
+                case this.bases[3]:
+                  this.result = parseInt(this.input, 8).toString(16);
+                  break;
+              }
+              break;
+            case this.bases[2]:
+              switch (this.baseTo) {
+                case this.bases[0]:
+                  this.result = parseInt(this.input, 10).toString(2);
+                  break;
+                case this.bases[1]:
+                  this.result = parseInt(this.input, 10).toString(8);
+                  break;
+                case this.bases[2]:
+                  this.result = this.input;
+                  break;
+                case this.bases[3]:
+                  this.result = parseInt(this.input, 10).toString(16);
+                  break;
+              }
+              break;
+            case this.bases[3]:
+              switch (this.baseTo) {
+                case this.bases[0]:
+                  this.result = parseInt(this.input, 16).toString(2);
+                  break;
+                case this.bases[1]:
+                  this.result = parseInt(this.input, 16).toString(8);
+                  break;
+                case this.bases[2]:
+                  this.result = parseInt(this.input, 16).toString(10);
+                  break;
+                case this.bases[3]:
+                  this.result = this.input;
+                  break;
+              }
+              break;
+            default:
+              break;
           }
         }
-      },
-
-      // The name is descriptive enough
-      decimalToBinary (number) {
-        return parseInt(number).toString(2); // Found this online
       },
 
       // Update Rules
@@ -73,6 +132,7 @@
         if (this.baseFrom == this.bases[0]) {
 
           // Set Base 2 Rule
+          // this.rules.push(v => /[0-1]/.test(v) || 'Number Must Be Less Than 2');
         } else if (this.baseFrom == this.bases[1]) {
 
           // Set Base 8 Rule

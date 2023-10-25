dcdc34e71890dfa80a2ef86e225ec191c5a15d18
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2023-10-25 15:29:09 +0100

    feat: input validations

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -4,14 +4,13 @@
       <v-form ref="form" class="my-5 text-center">
         <v-text-field
           solo
-          label="Number"
+          label="Number To Convert"
           clearable
           :hint="hint"
           append-icon="mdi-number"
           class="my-5"
           :rules="rules"
           v-model="input"
-          type="number"
         ></v-text-field>
         <v-row>
           <v-col cols="6">
@@ -53,7 +52,13 @@ export default {
       baseFrom: null,
       baseTo: null,
       bases: ["Binary (2)", "Octal (8)", "Decimal (10)", "Hexadecimal (16)"],
-      hint: "This is dynamic",
+      baseValues: {
+        "Binary (2)": 2,
+        "Octal (8)": 8,
+        "Decimal (10)": 10,
+        "Hexadecimal (16)": 16,
+      },
+      hint: "Number to convert",
       rules: [(v) => !!v || "Enter Number To Convert"],
       baseRules: [(v) => !!v || "Choose A Base"],
       result: 0,
@@ -67,89 +72,33 @@ export default {
 
       // Validate inputs
       if (this.$refs.form.validate()) {
-        // Lemme try this
-        switch (this.baseFrom) {
-          case this.bases[0]:
-            switch (this.baseTo) {
-              case this.bases[0]:
-                this.result = this.input;
-                break;
-              case this.bases[1]:
-                this.result = parseInt(this.input, 2).toString(8);
-                break;
-              case this.bases[2]:
-                this.result = parseInt(this.input, 2).toString(10);
-                break;
-              case this.bases[3]:
-                this.result = parseInt(this.input, 2).toString(16);
-                break;
-            }
-            break;
-          case this.bases[1]:
-            switch (this.baseTo) {
-              case this.bases[0]:
-                this.result = parseInt(this.input, 8).toString(2);
-                break;
-              case this.bases[1]:
-                this.result = this.input;
-                break;
-              case this.bases[2]:
-                this.result = parseInt(this.input, 8).toString(10);
-                break;
-              case this.bases[3]:
-                this.result = parseInt(this.input, 8).toString(16);
-                break;
-            }
-            break;
-          case this.bases[2]:
-            switch (this.baseTo) {
-              case this.bases[0]:
-                this.result = parseInt(this.input, 10).toString(2);
-                break;
-              case this.bases[1]:
-                this.result = parseInt(this.input, 10).toString(8);
-                break;
-              case this.bases[2]:
-                this.result = this.input;
-                break;
-              case this.bases[3]:
-                this.result = parseInt(this.input, 10).toString(16);
-                break;
-            }
-            break;
-          case this.bases[3]:
-            switch (this.baseTo) {
-              case this.bases[0]:
-                this.result = parseInt(this.input, 16).toString(2);
-                break;
-              case this.bases[1]:
-                this.result = parseInt(this.input, 16).toString(8);
-                break;
-              case this.bases[2]:
-                this.result = parseInt(this.input, 16).toString(10);
-                break;
-              case this.bases[3]:
-                this.result = this.input;
-                break;
-            }
-            break;
-          default:
-            break;
+        if (this.baseFrom === this.baseTo) {
+          this.result = this.input;
+        } else {
+          this.result = parseInt(
+            this.input,
+            this.baseValues[this.baseFrom]
+          ).toString(this.baseValues[this.baseTo]);
         }
       }
     },
 
     // Update Rules
     setRules() {
-      if (this.baseFrom == this.bases[0]) {
+      if (this.baseFrom === this.bases[0]) {
         // Set Base 2 Rule
-        // this.rules.push(v => /[0-1]/.test(v) || 'Number Must Be Less Than 2');
-      } else if (this.baseFrom == this.bases[1]) {
+        this.rules[1] = (v) => /[0-1]/.test(v) || "Number Must Be Less Than 2";
+      } else if (this.baseFrom === this.bases[1]) {
         // Set Base 8 Rule
-      } else if (this.baseFrom == this.bases[2]) {
+        this.rules[1] = (v) => /[0-7]/.test(v) || "Number Must Be Less Than 8";
+      } else if (this.baseFrom === this.bases[2]) {
         // Set Base 10 Rule
-      } else if (this.baseFrom == this.bases[3]) {
+        this.rules[1] = (v) =>
+          /[0-9]/.test(v) || "Only numeric characters are allowed";
+      } else if (this.baseFrom === this.bases[3]) {
         // Set Base 16 Rule
+        this.rules[1] = (v) =>
+          /[0-9A-Fa-f]/.test(v) || "Number Must Be Less Than 16";
       }
     },
   },
